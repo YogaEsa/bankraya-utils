0b0e8dafe4ff99d1c10db8ec33fcd26e10ff08f0
Yoga Esa Mahendra <yogaesamahendra@Yogas-MacBook-Air.local>
2025-03-25 10:41:34 +0700

    initial commit

---
+++ validation.go
@@ -0,0 +1,37 @@
+package validation
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/go-playground/validator"
+)
+
+// validateRequest memvalidasi struct request dan mengembalikan error jika ada
+func validateRequest(req interface{}) error {
+	var validate = validator.New()
+	if err := validate.Struct(req); err != nil {
+		var errorMessages []string
+		for _, err := range err.(validator.ValidationErrors) {
+			errorMessages = append(errorMessages, formatValidationError(err))
+		}
+		return errors.New(strings.Join(errorMessages, ", "))
+	}
+	return nil
+}
+
+// formatValidationError membuat pesan error lebih spesifik
+func formatValidationError(fe validator.FieldError) string {
+	switch fe.Tag() {
+	case "required":
+		return fe.Field() + " tidak boleh kosong"
+	case "email":
+		return fe.Field() + " harus berupa email yang valid"
+	case "min":
+		return fe.Field() + " harus memiliki minimal " + fe.Param() + " karakter"
+	case "max":
+		return fe.Field() + " tidak boleh lebih dari " + fe.Param() + " karakter"
+	default:
+		return fe.Field() + " tidak valid"
+	}
+}
